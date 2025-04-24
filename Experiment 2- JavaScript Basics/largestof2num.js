dbde1fc05532d9f8be57ea4c4d9c6201d1b0e09e
Adeela Azeez <143096306+CRICSTAR1718@users.noreply.github.com>
2025-04-24 21:52:11 +0530

    Add files via upload

---
+++ Experiment 2- JavaScript Basics/largestof2num.js
@@ -0,0 +1,9 @@
+function largest(a,b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+console.log(`The largest of given numbers is: `+largest(8,2))
---
+++ Experiment 2- JavaScript Basics/tenth.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    let numbers = [10, 20, 30, 40, 50];
+        document.getElementById("demo").innerHTML = numbers.join(" , ");
+  }
